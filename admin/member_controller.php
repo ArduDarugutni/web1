77c3c3f727a6739e68f2d7780b0416317d5bb787
ArduDarugutni <theviel77@gmail.com>
2023-05-13 00:16:51 +0700

    UP

--- admin/member_controller.php
+++ admin/member_controller.php
@@ -15,7 +15,7 @@ $model = new Member();
 $rs = $model->ceklogin($data);
 if (!empty($rs)) {
     $_SESSION['MEMBER'] = $rs;
-    header('Location:http://localhost/web1/Admin/index.php?url=product');
+    header('Location:http://localhost/web1/admin/index.php?url=product');
 } else {
     echo '<script> alert("user/password anda salah"); history.back();</script>';
 }
